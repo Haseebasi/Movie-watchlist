703e1caa62d4a77104f1da05373dac01731f2563
Haseebasi <Haseebasi005@gmail.com>
2026-05-29 11:01:17 +0530

    made watchlist js to make the watchlist dynamic

--- watchlist.js
+++ watchlist.js
@@ -1,2 +1,16 @@
 const watchList = (JSON.parse(localStorage.getItem("watchList")))
-console.log(watchList)
+const emptyText = document.getElementById("empty-text") 
+const main = document.getElementById("mainSection")
+if(watchList && watchList.length >0){
+    emptyText.classList.add("hide")
+    for(i=0;i<watchList.length;i++){
+                renderEach(watchList[i])
+            }
+   
+}else{
+    emptyText.classList.remove("hide")
+}
+
+function renderEach(movieObj){
+    console.log(movieObj)
+}
